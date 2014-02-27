35f1a402722a5782b598c324d27c2b0917e61f69
Justin Jaffray <justinjaffray@khanacademy.org>
2014-02-26 21:26:17 -0800

    wow such input

--- skrit.js
+++ skrit.js
@@ -8,7 +8,6 @@ Skrit = (function() {
             }
         }
     };
-
     // shallowly copy all the properties on b onto a
     var extend = function(a, b) {
         for (var prop in b) {
@@ -18,51 +17,107 @@ Skrit = (function() {
         }
     };
     return {
-        createGame: function(constructor) {
+        // Create a singleton representing a game.
+        createGame: function(spec) {
             // The currently active world in the game
             var activeWorld;
 
             var stage;
             var renderer;
 
-            var stage = new PIXI.Stage(constructor.backgroundColor);
-            var renderer = new PIXI.WebGLRenderer(constructor.width, constructor.height);
-            constructor.container.appendChild(renderer.view);
+            var stage = new PIXI.Stage(spec.backgroundColor);
+            var renderer = new PIXI.WebGLRenderer(spec.width, spec.height);
+            spec.container.appendChild(renderer.view);
             requestAnimFrame(animate);
 
 
             function animate() {
-                requestAnimFrame( animate );
+                requestAnimFrame(animate);
                 renderer.render(stage);
-                if (activeWorld.update) activeWorld.update();
+                activeWorld.update();
+                activeWorld.render();
             }
 
+            // inputty stuff
+
+            var keys = [];
+            spec.container.addEventListener("keydown", function(e) {
+                keys[e.keyCode] = true;
+            });
+            spec.container.addEventListener("keyup", function(e) {
+                keys[e.keyCode] = false;
+            });
+
             return {
-                addChild: function(e) { stage.addChild(e) },
+                clearChildren: function(e) {
+                    for (var i = 0; i < stage.children.length; i++) {
+                        stage.removeChild(stage.children[i]);
+                    }
+                },
+                addChild: function(e) { stage.addChild(e); },
                 setActiveWorld: function(world) {
                     activeWorld = world;
-                }
+                    world.game = this;
+                },
+                Input: {
+                    // who uses other keys
+                    // TODO add other keys
+                    LEFT: 37,
+                    UP:  38,
+                    RIGHT: 39,
+                    DOWN: 40,
+                },
+                keys: keys
             };
         },
-        createWorld: function(constructor) {
+        createWorld: function(spec) {
             var World = function () {
                 this.entities = [];
             }
             World.prototype.add = function(ent) {
                 this.entities.push(ent);
+                ent.world = this;
+                ent.game = this.game;
+                ent._born();
+                if (ent.born) ent.born();
             };
+            // Called when the world is set to the active world in a game.
+            World.prototype._birth = function() {
+            };
+            // Called every frame of the game.
             World.prototype._update = function(ent) {
                 for (var i = 0; i < this.entities.length; i++) {
                     this.entities[i].update();
                 }
             };
+            // If you override me you have to tell me to update the entities...
             World.prototype.update = World.prototype._update;
-            extend(World.prototype, constructor);
+            World.prototype._render = function() {
+                var entity;
+                // efficiency? we don't need efficiency where we're going.
+                // there is probably a better library for this approach, but
+                // yolo for now
+                this.game.clearChildren();
+                for (var i = 0; i < this.entities.length; i++) {
+                    entity = this.entities[i];
+                    entity.sprite.x = entity.x;
+                    entity.sprite.y = entity.y;
+                    this.game.addChild(this.entities[i].sprite);
+                }
+            };
+            World.prototype.render = World.prototype._render;
+            extend(World.prototype, spec);
             return World;
         },
-        createEntity: function(constructor) {
+        createEntity: function(spec) {
             var Ent = function () {}
-            extend(Ent.prototype, constructor);
+            Ent.prototype._born = function() {
+                this.texture = PIXI.Texture.fromImage(spec.image);
+                this.sprite = new PIXI.Sprite(this.texture);
+                this.x = 0;
+                this.y = 0;
+            };
+            extend(Ent.prototype, spec);
             return Ent;
         }
     }
